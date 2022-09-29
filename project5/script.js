1d08bd069ed3e386303ed72596ba9be654f1fcb6
Muhammad Hamza <82886878+hamzakhan0153@users.noreply.github.com>
2022-09-29 17:03:41 +0500

    complete

--- project5/script.js
+++ project5/script.js
@@ -1,21 +1,97 @@
-const menuToggle = document.getElementById('toggle');
-const close = document.getElementById('close');
-const open = document.getElementById('open');
-const modal = document.getElementById('modal');
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const filterBtn = document.getElementById('filter');
+const sortBtn = document.getElementById('sort');
+const sumBtn = document.getElementById('sum');
 
-// Event Listeners
-// 1. Listen for click on toggle button
-menuToggle.addEventListener('click', () => {
-    document.body.classList.toggle('show-nav')
-})
+let data = [];
 
-// 2. Listen for click on open button
-open.addEventListener('click', () => modal.classList.add('show-modal') );
+ getRamdomUser  = async () =>  {
+    const res = await axios.get('https://randomuser.me/api/')
+    const data = await res.data;
+    //console.log(data);
+    const user = data.results[0];
+    //console.log(user);
 
-// 3. Listen for click on close button
-close.addEventListener('click', () => modal.classList.remove('show-modal') );
+    const newUser = {
+        name: `${user.name.title} ${user.name.first} ${user.name.last}`,
+        balance: Math.floor(Math.random()*1000000)
+    }
+    //console.log(newUser)
 
-// 4. Listen for click outside of modal
-window.addEventListener('click', e => 
-    e.target === modal ? modal.classList.remove('show-modal') : false
-)
+    addData(newUser);
+};
+
+ addData = (newUser) => {
+    
+    data.push(newUser);
+    console.log('data array',data);
+    updateDOM();
+
+ }
+
+ doubleMoney = () => {
+    data = data.map(user => {
+        return { ...user, balance:user.balance * 2 }
+    });
+
+    updateDOM();
+ }
+
+ filterUsers = () => {
+    data = data.filter(user => user.balance >= 1000000);
+    updateDOM();
+ }
+
+ sortByBalance = () => {
+    data = data.sort((a,b) => b.balance - a.balance);
+    updateDOM();
+ }
+
+ formatNumberToDollar = (number) => {
+     return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+ }
+
+ totalBalance = () => {
+    //add value in accumuulator
+    const balance = data.reduce((acc, user) => (acc += user.balance), 0);
+    const balanceElement  = document.createElement('div');
+    balanceElement.innerHTML = `<h3>Total Balance: ${formatNumberToDollar(balance)}</h3>`
+    main.appendChild(balanceElement)
+    //updateDOM();
+ }
+
+ updateDOM = (userData = data) => {
+    //clear ui
+    main.innerHTML = '<h2><strong>User</strong>Wealth</h2>'
+    userData.forEach(user => {
+        //new div
+        const userDiv = document.createElement('div');
+        userDiv.classList.add('user');
+        userDiv.innerHTML = `<strong>${user.name}</strong>$
+        ${formatNumberToDollar(user.balance)}`
+        main.appendChild(userDiv)
+        //console.log(user);
+    })
+}
+
+ addUserBtn.addEventListener('click', getRamdomUser);
+
+ doubleBtn.addEventListener('click', doubleMoney)
+
+ filterBtn.addEventListener('click', filterUsers)
+
+ sortBtn.addEventListener('click', sortByBalance);
+
+ sumBtn.addEventListener('click', totalBalance);
+
+
+
+getRamdomUser();
+getRamdomUser();
+getRamdomUser();
+getRamdomUser();
+getRamdomUser();
+getRamdomUser();
+getRamdomUser();
